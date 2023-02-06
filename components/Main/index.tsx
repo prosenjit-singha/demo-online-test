e12ce6a392503e172ade9de43d842234e27ed388
PJAheibam <prosenjitsingha28@gmail.com>
2023-02-07 00:23:07 +0600

    button animation added

--- components/Main/index.tsx
+++ components/Main/index.tsx
@@ -1,17 +1,71 @@
 import Content from "./Content";
 import { TbArrowNarrowRight as NextIcon } from "react-icons/tb";
 import SidePanel from "./SidePanel";
+import { motion, MotionProps, Variants } from "framer-motion";
+
+const buttonsVariants: Variants = {
+  visible: {
+    transition: {
+      staggerChildren: 0.25,
+      delayChildren: 0.75,
+    },
+  },
+  hidden: {
+    transition: {
+      staggerChildren: 0.05,
+      delayChildren: -1,
+    },
+  },
+};
+
+const buttonsProps: MotionProps = {
+  variants: buttonsVariants,
+  initial: "hidden",
+  animate: "visible",
+};
+
+const buttonVariants: Variants = {
+  visible: {
+    opacity: 1,
+    scale: 1,
+  },
+  hidden: { opacity: 0, scale: 0, boxShadow: "none" },
+};
+
+const buttonProps: MotionProps = {
+  variants: buttonVariants,
+  whileHover: {
+    scale: 1.05,
+    boxShadow: "0 10px 15px -3px hsla(221, 83%, 53%, 0.75)",
+    transition: {
+      type: "tween",
+      ease: "easeOut",
+    },
+  },
+};
+
 export default function Main() {
   return (
     <main className="grid grid-cols-12 min-h-[calc(100vh-56px)] px-4 py-2 mb-4">
       <article className=" col-start-1 col-end-[-1] lg:col-end-10">
         {/* HEADER SECTION */}
-        <section className="flex items-center mb-4 gap-3">
-          <button className="btn-primary filled">All Section</button>
-          <button className="btn-primary">Physics</button>
-          <button className="btn-primary">Chemistry</button>
-          <button className="btn-primary">Maths</button>
-        </section>
+        <motion.section
+          {...buttonsProps}
+          className="flex items-center mb-4 gap-3"
+        >
+          <motion.button {...buttonProps} className="btn-primary filled">
+            All Section
+          </motion.button>
+          <motion.button {...buttonProps} className="btn-primary">
+            Physics
+          </motion.button>
+          <motion.button {...buttonProps} className="btn-primary">
+            Chemistry
+          </motion.button>
+          <motion.button {...buttonProps} className="btn-primary">
+            Maths
+          </motion.button>
+        </motion.section>
         {/* CONTENT SECTION */}
         <Content />
         {/* CONTENT ACTIONS */}
